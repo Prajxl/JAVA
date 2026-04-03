1ca2728cf00a4c3f02473641bda94ba602c46c93
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2026-04-03 22:48:52 +0530

    Linear Search in Array

--- src/Array/SecondLargest.java
+++ src/Array/SecondLargest.java
@@ -1,4 +1,23 @@
 package Array;
 
 public class SecondLargest {
+    static int SecondlargestInArray(int[] arr)
+    {
+        int largest=0;
+        int secondLarge=0;
+        for (int i=0;i<arr.length;i++)
+        {
+            if(largest<arr[i])
+            {
+                secondLarge=largest;
+                largest=arr[i];
+            }
+        }
+        return secondLarge;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={5,2,1,2,7,4,9,10};
+        System.out.println(SecondlargestInArray(arr));
+    }
 }
